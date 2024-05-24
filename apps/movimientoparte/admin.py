366f35b1ae96c13827cd73adac8f9a3fe5442d76
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-23 19:44:25 -0500

    Se cambia el orden en el admin de movimiento partes para que se muestre de primero la parte del wod que se va a trabajar

--- apps/movimientoparte/admin.py
+++ apps/movimientoparte/admin.py
@@ -5,4 +5,4 @@ from apps.movimientoparte.models import MovimientoParte
 
 @admin.register(MovimientoParte)
 class MovimientoParteAdmin(admin.ModelAdmin):
-    list_display = ['fk_parte_evento', 'fk_movimiento', 'repeticiones', 'orden','date_created' ]
+    list_display = ['orden', 'fk_parte_evento', 'fk_movimiento', 'repeticiones', 'date_created' ]
