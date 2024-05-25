5a507fc1aeda0d4a60735961e626982eb81ea1b8
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-25 11:08:28 -0500

    Se agrega accesToken de 24 horas y accessRefresh de 7 dias

--- SFC/settings.py
+++ SFC/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,6 +144,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(hours=24),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7),
 }
 
 # Static files (CSS, JavaScript, Images)
