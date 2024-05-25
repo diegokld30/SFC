5293651992909b998989b92402e0f134a21194ee
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-25 11:24:14 -0500

    Se carga el serializador de categoria el cual permite subir cambios de una FK, tambien añadi lo necesario para que sea unico el registro en la base de datos

--- apps/categoria/api/serializers.py
+++ apps/categoria/api/serializers.py
@@ -8,3 +8,9 @@ class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ['id', 'nombre', 'fk_competencia', 'date_created', 'date_modified']
+
+    def create(self, validated_data):
+        competencia_data = validated_data.pop('fk_competencia')
+        competencia, created = Competicion.objects.get_or_create(**competencia_data)
+        categoria = Categoria.objects.create(fk_competencia=competencia, **validated_data)
+        return categoria
--- apps/categoria/models.py
+++ apps/categoria/models.py
@@ -3,7 +3,7 @@ from django.db.models import SET_NULL
 from apps.competicion.models import Competicion
 
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, unique=True)
     fk_competencia = models.ForeignKey(Competicion, on_delete=SET_NULL, null = True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
