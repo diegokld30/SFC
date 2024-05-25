f4810e82f94ed76eb7cae8d2a4f4a285a37b058a
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-25 12:57:16 -0500

    Se agrega metodo de actualizar en categoria

--- apps/categoria/api/serializers.py
+++ apps/categoria/api/serializers.py
@@ -14,3 +14,12 @@ class CategoriaSerializer(serializers.ModelSerializer):
         competencia, created = Competicion.objects.get_or_create(**competencia_data)
         categoria = Categoria.objects.create(fk_competencia=competencia, **validated_data)
         return categoria
+
+    def update(self, instance, validated_data):
+        instance.nombre = validated_data.get('nombre', instance.nombre)
+        competencia_data = validated_data.pop('fk_competencia', None)
+        if competencia_data:
+            competencia, created = Competicion.objects.get_or_create(**competencia_data)
+            instance.fk_competencia = competencia
+        instance.save()
+        return instance
