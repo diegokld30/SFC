161572d6fcd151465ad082b1d640ba4c497fd977
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-25 12:16:14 -0500

    Se movio la configuracion de cors en middleware

--- SFC/settings.py
+++ SFC/settings.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'SFC.urls'
 
 TEMPLATES = [
