5c6df9afc984b03a1cfb6c78bd5b3668b012084a
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-25 13:04:20 -0500

    Se quitaron los permisos para que pueda actualizar

--- apps/competicion/api/views.py
+++ apps/competicion/api/views.py
@@ -4,6 +4,6 @@ from apps.competicion.models import Competicion
 from apps.competicion.api.permissions import IsOwnerOrReadOnly
 
 class CompeticionViewSet(ModelViewSet):
-    permission_classes = [IsOwnerOrReadOnly]
+    #permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CompeticionSerializer
     queryset = Competicion.objects.all()
