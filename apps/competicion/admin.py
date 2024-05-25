8575aef99984ab20a71ba33281c21488f6d392b8
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-25 15:46:02 -0500

    Agregue fecha de actualizacion a la creacion de competicion

--- apps/competicion/admin.py
+++ apps/competicion/admin.py
@@ -3,5 +3,5 @@ from apps.competicion.models import Competicion
 
 @admin.register(Competicion)
 class CompeticionAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'state', 'date_created', 'date_finished', 'date_finished']
+    list_display = ['nombre', 'state', 'date_created', 'date_finished', 'date_updated']
 
