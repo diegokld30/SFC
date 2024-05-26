69fdc44d0555da8e84e0d95d3df72dbe9d7c9a34
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-26 10:08:05 -0500

    Se modifico lo necesario para lograr insertar un evento sin un manejo complejo de objetos, ya que estaba presentando error a la hora de insertar las llaves foraneas

--- apps/categoria/admin.py
+++ apps/categoria/admin.py
@@ -3,4 +3,4 @@ from apps.categoria.models import Categoria
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nombre', 'fk_competencia', 'date_created', 'date_modified']
+    list_display = ['nombre', 'fk_competencia', 'date_created', 'date_modified']
--- apps/evento/api/views.py
+++ apps/evento/api/views.py
@@ -2,7 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 from apps.evento.api.serializers import EventoSerializer
 from apps.evento.models import Evento
 
-
 class EventoViewSet(ModelViewSet):
     serializer_class = EventoSerializer
     queryset = Evento.objects.all()
