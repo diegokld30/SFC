81f880d9a7cf54b7148ba1c1880df93e39d212a7
diegokld30 <dfercalderon@poligran.edu.co>
2024-05-28 08:26:44 -0500

    Se creo un metodo para lograr insertar parteEvento ya que estaba generando errores a la hora de insertar por la llaves foraneas que se modificaron en evento

--- apps/parteevento/api/serializers.py
+++ apps/parteevento/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from apps.parteevento.models import ParteEvento
+from apps.evento.models import Evento
 from apps.evento.api.serializers import EventoSerializer
 
 class ParteEventoSerializer(serializers.ModelSerializer):
@@ -8,3 +9,9 @@ class ParteEventoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParteEvento
         fields = ['id', 'fk_evento', 'orden', 'timecap', 'descanso', 'date_created']
+
+    def create(self, validated_data):
+        fk_evento_data = validated_data.pop('fk_evento')
+        fk_evento = Evento.objects.create(**fk_evento_data)
+        parte_evento = ParteEvento.objects.create(fk_evento=fk_evento, **validated_data)
+        return parte_evento
