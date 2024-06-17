84a556c15858e05414df7310455f07c0ef51d855
diegokld30 <dfercalderon@poligran.edu.co>
2024-06-17 17:46:42 -0500

    Modifique el metodo create para que reciba unicamente el id como int en las fk

--- apps/usuarioEvento/api/serializers.py
+++ apps/usuarioEvento/api/serializers.py
@@ -2,15 +2,20 @@ from rest_framework import serializers
 from apps.usuarioEvento.models import usuarioEvento
 from apps.user.models import User
 from apps.evento.models import Evento
-from apps.user.api.serializers import UserSerializer
-from apps.evento.api.serializers import EventoSerializer
+from rest_framework.serializers import PrimaryKeyRelatedField
 
 class UsuarioEventoSerializer(serializers.ModelSerializer):
-    fk_user = UserSerializer()
-    fk_evento = EventoSerializer()
-    #fk_user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    #fk_evento = serializers.PrimaryKeyRelatedField(queryset=Evento.objects.all())
+    fk_user = PrimaryKeyRelatedField(queryset=User.objects.all())
+    fk_evento = PrimaryKeyRelatedField(queryset=Evento.objects.all())
+
 
     class Meta:
         model = usuarioEvento
         fields = ['id','fk_user','fk_evento','conteo_reps','date_start','date_end','date_modified']
+
+    def create(self, validated_data):
+        fk_user = validated_data.pop('fk_user')
+        fk_evento = validated_data.pop('fk_evento')
+
+        usuario_evento = usuarioEvento.objects.create(fk_user=fk_user, fk_evento=fk_evento, **validated_data)
+        return usuario_evento
