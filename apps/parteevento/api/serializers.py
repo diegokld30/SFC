f7cc6506177261cd09285e2a57ca021c86f1847a
diegokld30 <dfercalderon@poligran.edu.co>
2024-06-17 17:30:22 -0500

    Se cambia para que ls fk sea enviada como int

--- apps/parteevento/api/serializers.py
+++ apps/parteevento/api/serializers.py
@@ -1,17 +1,16 @@
 from rest_framework import serializers
+from rest_framework.serializers import PrimaryKeyRelatedField
 from apps.parteevento.models import ParteEvento
 from apps.evento.models import Evento
-from apps.evento.api.serializers import EventoSerializer
 
 class ParteEventoSerializer(serializers.ModelSerializer):
-    fk_evento = EventoSerializer()
+    fk_evento = PrimaryKeyRelatedField(queryset=Evento.objects.all())
 
     class Meta:
         model = ParteEvento
         fields = ['id', 'fk_evento', 'orden', 'timecap', 'descanso', 'date_created']
 
     def create(self, validated_data):
-        fk_evento_data = validated_data.pop('fk_evento')
-        fk_evento = Evento.objects.create(**fk_evento_data)
+        fk_evento = validated_data.pop('fk_evento')
         parte_evento = ParteEvento.objects.create(fk_evento=fk_evento, **validated_data)
         return parte_evento
